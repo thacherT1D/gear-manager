3642fa1f3062868492a12796b24d5e814987ae9a
Thacher Hussain <thacherT1D@gmail.com>
2019-04-17 16:58:02 -0700

    assets for icon and splash screen, structure and components for form from native base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Container, Header, Left, Body, Right, Title, Content, Text } from 'native-base';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import firebase from 'firebase';
 
+import SignInForm from './components/SignInForm';
 import reducers from './reducers';
 
 class App extends Component {
   componentWillMount() {
     const config = {
-    apiKey: 'AIzaSyA2UZSQAq1kxUlr4Q7WNIJYGZl5G2HBnug',
-    authDomain: 'gear-manager-25100.firebaseapp.com',
-    databaseURL: 'https://gear-manager-25100.firebaseio.com',
-    projectId: 'gear-manager-25100',
-    storageBucket: 'gear-manager-25100.appspot.com',
-    messagingSenderId: '355491663632'
-  };
-  
-  firebase.initializeApp(config);
+      apiKey: 'AIzaSyA2UZSQAq1kxUlr4Q7WNIJYGZl5G2HBnug',
+      authDomain: 'gear-manager-25100.firebaseapp.com',
+      databaseURL: 'https://gear-manager-25100.firebaseio.com',
+      projectId: 'gear-manager-25100',
+      storageBucket: 'gear-manager-25100.appspot.com',
+      messagingSenderId: '355491663632'
+    };
+
+    firebase.initializeApp(config);
   }
 
   render() {
@@ -33,12 +34,27 @@ class App extends Component {
         </Header>
         <Content padder>
           <Provider store={createStore(reducers)}>
-            <Text>Hello there gear junkie!</Text>
+            <View style={styles.title}>
+              <Text style={styles.text}>Hello there gear junkie!</Text>
+            </View>
+            <SignInForm />
           </Provider>
         </Content>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 20
+  },
+  title: {
+    alignSelf: 'center'
+  },
+  text: {
+    fontSize: 20
+  }
+});
 
 export default App;
---
+++ src/components/SignInForm.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import { Content, Form, Item, Input, Button, Text } from 'native-base';
+
+class SignInForm extends Component {
+  render() {
+    return (
+      <Content>
+        <Form>
+          <Item>
+            <Input placeholder="Email" />
+          </Item>
+          <Item last>
+            <Input placeholder="Password" />
+          </Item>
+        </Form>
+        <Button block style={[styles.button]}>
+          <Text>Button</Text>
+        </Button>
+      </Content>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  button: {
+    marginTop: 20,
+  },
+  title: {
+    justifyContent: 'center'
+  }
+});
+
+export default SignInForm;
