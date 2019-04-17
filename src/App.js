3247411c7f298c64676de536c30a85452f07e794
Thacher Hussain <thacherT1D@gmail.com>
2019-04-17 16:16:59 -0700

    firebase setup

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import React, { Component } from 'react';
 import { Container, Header, Left, Body, Right, Title, Content, Text } from 'native-base';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+    apiKey: 'AIzaSyA2UZSQAq1kxUlr4Q7WNIJYGZl5G2HBnug',
+    authDomain: 'gear-manager-25100.firebaseapp.com',
+    databaseURL: 'https://gear-manager-25100.firebaseio.com',
+    projectId: 'gear-manager-25100',
+    storageBucket: 'gear-manager-25100.appspot.com',
+    messagingSenderId: '355491663632'
+  };
+  
+  firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Container>
