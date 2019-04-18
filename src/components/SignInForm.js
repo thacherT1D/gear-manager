0ef94078f4cc84ed0e00020365735780763490b0
Thacher Hussain <thacherT1D@gmail.com>
2019-04-17 23:23:48 -0700

    redux setup, format and styling

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,34 +1,68 @@
 import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
-import { Content, Form, Item, Input, Button, Text } from 'native-base';
+import { StyleSheet, View } from 'react-native';
+import {
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+  Button,
+  Text
+} from 'native-base';
+import { connect } from 'react-redux';
+import { emailChanged, passwordChanged } from '../actions';
 
 class SignInForm extends Component {
+  onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
+
+  onPasswordChange(text) {
+    this.props.passwordChanged(text);
+  }
+
   render() {
     return (
-      <Content>
+      <View>
         <Form>
-          <Item>
-            <Input placeholder="Email" />
+          <Item floatingLabel>
+            <Label>Email</Label>
+            <Input
+              onChangeText={this.onEmailChange.bind(this)}
+              value={this.props.email}
+              />
           </Item>
-          <Item last>
-            <Input placeholder="Password" />
+          <Item floatingLabel>
+            <Label>Password</Label>
+            <Input
+              secureTextEntry
+              onChangeText={this.onPasswordChange.bind(this)}
+              value={this.props.password}
+             />
           </Item>
         </Form>
-        <Button block style={[styles.button]}>
+        <Button info block style={[styles.button]}>
           <Text>Sign In</Text>
         </Button>
-      </Content>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   button: {
-    marginTop: 20,
+    marginTop: 30,
   },
   title: {
     justifyContent: 'center'
   }
 });
 
-export default SignInForm;
+const mapStateToProps = state => {
+  return {
+    email: state.auth.email,
+    password: state.auth.password
+  };
+};
+
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(SignInForm);
