118d84bf897aee6c610ccd29329256d2088a05cb
Thacher Hussain <thacherT1D@gmail.com>
2019-04-20 15:58:28 -0700

    linting updates

--- App.js
+++ App.js
@@ -1,15 +1,4 @@
-// import { AppRegistry } from 'react-native';
-//
-// import App from './src/App';
-//
-// AppRegistry.registerComponent('gear-manager', () => App);
-//
-// export default App;
-//
-
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Container, Content } from 'native-base';
 
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
@@ -17,19 +6,10 @@ import HomeScreen from './src/screens/HomeScreen';
 import GearListScreen from './src/screens/GearListScreen';
 
 const MainNavigator = createStackNavigator({
-  Home: {screen: HomeScreen},
-  GearList: {screen: GearListScreen}
+  Home: { screen: HomeScreen },
+  GearList: { screen: GearListScreen }
 });
 
 const App = createAppContainer(MainNavigator);
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
